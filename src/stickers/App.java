efc2e6d640bf0097fcbee6c8577e5bac69f65fb2
Kennedy Brito <brito.kennedy10@gmail.com>
2022-07-21 20:01:12 -0300

    feito geradora de figurinhas

--- src/stickers/App.java
+++ src/stickers/App.java
@@ -1,7 +1,8 @@
 package stickers;
 
-import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -10,7 +11,9 @@ import java.util.List;
 import java.util.Map;
 
 public class App {
-	public static void main(String[] args) throws IOException, InterruptedException {
+
+	
+	public static void main(String[] args) throws Exception {
 
 	String url = "https://alura-filmes.herokuapp.com/conteudos";
 	URI uri = URI.create(url);
@@ -26,10 +29,27 @@ public class App {
 	JsonParse parser = new JsonParse();
 	List<Map<String, String>> listadeFilmes = parser.parse(body);
 	
-	System.out.println(listadeFilmes.get(1));
+
 	
 	for (Map<String, String> map : listadeFilmes) {
-		System.out.println("O nome do filme e " + map.get("title"));
+		
+		// pegando a imagem na url 
+		String urlImagem = map.get("image");
+		String titulo = map.get("title");
+		
+		// nomeando a imagen nova 
+		InputStream inputStream = new URL(urlImagem).openStream();
+		String nomeArquivo = titulo + ".png";
+		
+		// gerando a imagem
+		GeradoraDeFigurinhas geradora = new GeradoraDeFigurinhas();
+		geradora.cria(inputStream, nomeArquivo);
+		
+		
+		
+		
+		
+		System.out.println( " O nome do filme e  " + map.get("title"));
 		System.out.println("foi lançado em  " + map.get("year"));
 		System.out.println();
 		
